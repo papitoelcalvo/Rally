c478b918dd5160c2b06069be5f004adfb2e05740
papitoelcalvo <darryfernandezmarmo@gmail.com>
2025-04-08 08:33:56 -0400

    se creo el proyecto

---
+++ frontend/src/app/Hola/page.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import { useState } from 'react';
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return <div>hola</div>
+}
+
+export default Page
