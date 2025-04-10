73890502873075eb8bd1742bb1b9a1859117f9cc
papitoelcalvo <darryfernandezmarmo@gmail.com>
2025-04-10 21:28:42 +0200

    I created some triangles

--- rally/src/app/Components/Rally.tsx
+++ rally/src/app/Components/Rally.tsx
@@ -1,19 +1,25 @@
-import { NextPage } from 'next'
+import { NextPage } from "next";
 
 interface Props {}
 
 const Rally: NextPage<Props> = ({}) => {
-  return <div className=''>
-    <div className='w-full h-[100vh] pt-[10vh]'>
-        <div className='w-full h-[90vh] bg-black'></div>
+  return (
+    <div className="">
+      <div className="w-full h-[100vh] pt-[10vh]">
+        <div className="w-full h-[90vh] bg-black"></div>
+      </div>
+      <div className="w-full h-[100vh] pt-[10vh]">
+        <div className="w-full h-[90vh] bg-black"></div>
+      </div>
+      <div className="w-full h-[100vh] pt-[10vh]">
+        <div className="w-full h-[90vh] flex ">
+          <div className="w-0 h-0 border-b-[400px] border-b-red-500 border-l-[200px] border-l-transparent border-r-[200px] border-r-transparent -rotate-90"></div>
+          <div className="w-0 h-0 border-b-[400px] border-b-red-500 border-l-[200px] border-l-transparent border-r-[200px] border-r-transparent rotate-180"></div>
+          <div className="w-0 h-0 border-b-[400px] border-b-red-500 border-l-[200px] border-l-transparent border-r-[200px] border-r-transparent "></div>
+        </div>
+      </div>
     </div>
-    <div className='w-full h-[100vh] pt-[10vh]'>
-        <div className='w-full h-[90vh] bg-black'></div>
-    </div>
-    <div className='w-full h-[100vh] pt-[10vh]'>
-        <div className='w-full h-[90vh] bg-black'></div>
-    </div>
-  </div>
-}
+  );
+};
 
-export default Rally
+export default Rally;
--- rally/src/app/page.tsx
+++ rally/src/app/page.tsx
@@ -4,16 +4,10 @@ import Rally from "./Components/Rally";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <div className="h-[100vh] overflow-auto">
       <NavBar/>
       <Rally/>
       <Foother/>
     </div>
-=======
-    <>
-        <h1 className="text-3xl font-bold justify-center flex">Hello world!</h1>
-    </>
->>>>>>> James
   );
 }
