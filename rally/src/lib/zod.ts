b7728537fa995aa37e2c49f8ad92b45415867b9e
DevJames <devyirbeltj@gmail.com>
2025-04-10 15:46:51 -0400

    Endpoints and structure

--- rally/src/lib/zod.ts
+++ rally/src/lib/zod.ts
@@ -8,9 +8,16 @@ export const LoginSchema = object({
     .min(1, "Username is required"),
     password: string()
     .min(1, "Password is required")
+});
+
+export const CreateSchema = object({
+    username: string()
+    .min(1, "Username is required"),
+    password: string()
+    .min(1, "Password is required")
     .min(8, "Password must be at least 8 characters")
     .regex(/[A-Z]/, "Must contain at least one uppercase letter")
     .regex(/[a-z]/, "Must contain at least one lowercase letter")
     .regex(/[0-9]/, "Must contain at least one number")
     .regex(/[^A-Za-z0-9]/, "Must contain at least one special character")
-});
+ });
